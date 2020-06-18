ea17bde92188f5f181f9dfdf5876f6936db8fc21
Daniel Pfeffer <daniel.pfeffer@breathless-pictures.at>
2020-06-19 01:27:46 +0200

    Initial Commit

---
+++ DrBooze/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrBooze
+//
+//  Created by Daniel Pfeffer on 19.06.20.
+//  Copyright © 2020 Daniel Pfeffer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
